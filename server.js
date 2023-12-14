25ccbea75dd85d79b30de783bba0946cb1942041
Ephraim Sib <97473989+EphraimSib@users.noreply.github.com>
2023-12-14 15:35:53 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,27 +1,33 @@
-// Dependencies
+const express = require('express');
 const axios = require('axios');
+const cors = require('cors');
 
-// Server port
-const PORT = 8080;
+const app = express();
+app.use(cors());
 
-// Create a basic HTTP server
-const server = require('http').createServer((req, res) => {
- res.writeHead(200, {'Content-Type': 'text/plain'});
+const igAPI = 'https://www.instagram.com/accounts/login/ajax/';
 
- // Fetch data from a RESTful API
- axios.get('https://jsonplaceholder.typicode.com/todos/1')
-    .then(response => {
-      // Output the data to the server response
-      res.end(JSON.stringify(response.data));
-    })
-    .catch(error => {
-      // Handle errors
-      console.error('Error fetching data:', error);
-      res.end('Error fetching data');
+app.get('/igbluebadge', async (req, res) => {
+ try {
+    const response = await axios.get(igAPI);
+    const igData = response.data;
+    if (igData.status === 'ok') {
+      const blueBadgeStatus = igData.authenticated;
+      res.json({
+        blueBadgeStatus,
+      });
+    } else {
+      throw new Error('An error occurred while retrieving IG data');
+    }
+ } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      error: 'Internal Server Error',
     });
+ }
 });
 
-// Start the server
-server.listen(PORT, () => {
- console.log(`Server running on port ${PORT}`);
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+ console.log(`Server is running on port ${PORT}`);
 });
