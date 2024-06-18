866edb702bd7e85205ee1af0fc080a753bb2bf20
Ephraim Sib <97473989+EphraimSib@users.noreply.github.com>
2024-06-18 13:40:15 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,69 @@
+const express = require('express');
+
+const axios = require('axios');
+
+const cors = require('cors');
+
+const http = require('http');
+
+const port = process.argv[2];
+
+
+
+const app = express();
+
+app.use(cors());
+
+
+
+const igAPI = 'https://www.instagram.com/accounts/login/ajax/';
+
+
+
+app.get('/igbluebadge', async (req, res) => {
+
+ try {
+
+    const response = await axios.get(igAPI);
+
+    const igData = response.data;
+
+    if (igData.status === 'ok') {
+
+      const blueBadgeStatus = igData.authenticated;
+
+      res.json({
+
+        blueBadgeStatus,
+
+      });
+
+    } else {
+
+      throw new Error('An error occurred while retrieving IG data');
+
+    }
+
+ } catch (error) {
+
+    console.error(error);
+
+    res.status(500).json({
+
+      error: 'Internal Server Error',
+
+    });
+
+ }
+
+});
+
+
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+
+ console.log(`Server is running on port ${PORT}`);
+
+});
